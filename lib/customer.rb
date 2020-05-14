2a13814e987c0c09bd6fe27a89f5756ea021195c
frod25 <frod25>
2020-05-14 15:45:25 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,32 @@
 class Customer
-  
+  attr_reader :name
+  attr_accessor :age
+
+    @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip)
+    meal = Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    Meal.all.select {|meal| meal.customer == self}
+  end
+
+  def waiters
+    self.meals.collect do |meal|
+        if meal.customer == self
+            meal.waiter
+        end
+    end
+  end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,36 @@
 class Waiter
+    attr_reader :name
+    attr_accessor :years_of_exp
 
+    @@all = []
+
+    def initialize(name, years_of_exp)
+        @name = name
+        @years_of_exp = years_of_exp
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        meal = Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|meal| meal.waiter == self}
+    end
+
+    def best_tipper
+        tipper = nil
+        best_tip = 0
+        self.meals.each do |meal|
+            if meal.tip > best_tip
+                best_tip = meal.tip
+                tipper = meal.customer
+            end
+        end
+        tipper
+    end
 end
