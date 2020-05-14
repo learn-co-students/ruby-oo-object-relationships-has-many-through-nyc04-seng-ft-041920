893bc2f6ecde2a6c0a7ee6399a40de2ab2ad30f1
JonTDean <JonathanTomDean@Gmail.com>
2020-05-14 18:19:38 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,52 @@
 class Customer
-  
+    attr_accessor :name, :age
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+    
+    def meals
+        Meal.all.select do |my_meal|
+            my_meal.customer == self
+        end
+    end
+
+    def waiters
+        waiter_array = []
+        meals.each do |meals|
+            if meals.customer == self
+                waiter_array << meals.waiter
+            end
+        end
+        
+        waiter_array
+    end
 end
+
+=begin
+1. Create a function #waiters that returns a new array of waiters that are associated with self
+    def waiters
+        waiter_array = []
+        meals.each do |meals|
+            if meals.customer == self
+                waiter_array << meals.waiter
+            end
+        end
+        
+        waiter_array
+    end
+
+2.
+
+=end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,41 @@
 class Waiter
+    attr_accessor :name, :experience
+    @@all = []
 
+    def initialize(name, experience)
+        @name = name 
+        @experience = experience
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end 
+
+    def meals
+        Meal.all.select do |meal|
+            meal.waiter == self
+        end
+    end
+
+    def best_tipper
+        tipper_string = nil
+        x = 0
+
+        meals.each do |meal|
+            if meal.waiter == self
+               if meal.tip > x
+                    x = meal.tip 
+                    tipper_string = meal.customer
+                end
+            end
+        end
+        
+        tipper_string
+    end
 end
