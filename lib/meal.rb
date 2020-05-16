defb0d531701d0077fa3d64534e540231ce5aa1e
“dinadeljanin” <ddeljanin5@icloud.com>
2020-05-15 20:53:38 -0400

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,22 @@
 class Meal
+  attr_reader :waiter, :customer, :total, :tip
+
+  @@all = []
+
+  def initialize(customer, total, tip=0, waiter)
+    @customer = customer
+    @total = total
+    @tip = tip
+    @waiter = waiter
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
